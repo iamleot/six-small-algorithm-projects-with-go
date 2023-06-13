832eb4ba5fdbcf68722e64c9cc20eea48af1da0d
Leonardo Taccari <iamleot@gmail.com>
2023-06-13 23:23:25 +0200

    Implement PrintArray function
    Add a function that print at maximum a certain number of elements in
    the array or just all the elements of the array.
    
    This should be the `print_array' function requested in `Sorting and
    Searching', first milestone `Bubble Sort'.

--- 01-sorting-and-searching/01-bubble-sort/utils/utils.go
+++ 01-sorting-and-searching/01-bubble-sort/utils/utils.go
@@ -1,6 +1,9 @@
 package utils
 
-import "math/rand"
+import (
+	"fmt"
+	"math/rand"
+)
 
 // MakeRandomArray creates a random int array with numItems elements
 // in the interval of [0, max).
@@ -12,4 +15,21 @@ func MakeRandomArray(numItems, max int) []int {
 	}
 
 	return a
+}
+
+// PrintArray print at maximum numItems of the int array.
+func PrintArray(array []int, numItems int) {
+	fmt.Println(sprintArray(array, numItems))
+}
+
+// sprintArray returns a string containing at maximum numItems elements of the
+// array.
+//
+// Only intended to make actual functionality PrintArray() testable.
+func sprintArray(array []int, numItems int) string {
+	if numItems >= len(array) {
+		return fmt.Sprint(array)
+	}
+
+	return fmt.Sprint(array[:numItems])
 }
--- 01-sorting-and-searching/01-bubble-sort/utils/utils_test.go
+++ 01-sorting-and-searching/01-bubble-sort/utils/utils_test.go
@@ -46,3 +46,73 @@ func TestMakeRandomArray(t *testing.T) {
 		})
 	}
 }
+
+func TestPrintArray(t *testing.T) {
+	PrintArray([]int{0, 1, 2}, 0)
+	// Output:
+	// []
+
+	PrintArray([]int{0, 1, 2}, 2)
+	// Output:
+	// [0 1]
+
+	PrintArray([]int{0, 1, 2}, 3)
+	// Output:
+	// [0 1 2]
+
+	PrintArray([]int{0, 1, 2}, 5)
+	// Output:
+	// [0 1 2]
+}
+
+func TestSprintArray(t *testing.T) {
+	type testCase struct {
+		array    []int
+		numItems int
+		expected string
+	}
+
+	var tests = map[string]testCase{
+		"zero elements array, 0 items to print": {
+			array:    []int{},
+			numItems: 0,
+			expected: "[]",
+		},
+		"zero elements array, 5 items to print": {
+			array:    []int{},
+			numItems: 5,
+			expected: "[]",
+		},
+		"five elements array, 0 items to print": {
+			array:    []int{0, 1, 2, 3, 4},
+			numItems: 0,
+			expected: "[]",
+		},
+		"five elements array, 3 items to print": {
+			array:    []int{0, 1, 2, 3, 4},
+			numItems: 3,
+			expected: "[0 1 2]",
+		},
+		"five elements array, 5 items to print": {
+			array:    []int{0, 1, 2, 3, 4},
+			numItems: 5,
+			expected: "[0 1 2 3 4]",
+		},
+		"five elements array, 10 items to print": {
+			array:    []int{0, 1, 2, 3, 4},
+			numItems: 10,
+			expected: "[0 1 2 3 4]",
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			printedArray := sprintArray(tc.array, tc.numItems)
+
+			if printedArray != tc.expected {
+				t.Errorf("expected: %q, got: %q",
+					tc.expected, printedArray)
+			}
+		})
+	}
+}
